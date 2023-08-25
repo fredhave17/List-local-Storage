80c9facb4a43e9080d887e08074ee9f6b244b916
Frederico Filho <fredhave17@gmail.com>
2023-08-24 23:02:29 -0300

    feat: created players screen

---
+++ listLocalStorage/src/screens/players/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Container, Form } from "./styles";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+
+export function Players() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight
+        title={"Nome da Turma"}
+        subtitle={"Adicione a galera e separe os times"}
+      />
+      <Form>
+        <Input placeholder='Nome da pessoa' autoCorrect={false} />
+        <ButtonIcon icon={"add"} />
+      </Form>
+      <Filter title='time A' isActive={true} />
+    </Container>
+  );
+}
